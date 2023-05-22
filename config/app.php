05c925e04d9d00c0863607bcd9040321fb2051cd
root <root@172-105-59-231.ip.linodeusercontent.com>
2023-05-22 12:10:19 +0000

    Changes to the users table

--- config/app.php
+++ config/app.php
@@ -181,11 +181,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-<<<<<<< HEAD
-        Maatwebsite\Excel\ExcelServiceProvider::class,
-=======
-       
->>>>>>> origin/master
 
         /*
          * Package Service Providers...
