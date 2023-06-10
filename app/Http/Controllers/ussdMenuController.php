83d11d9a6452b10b98b65a86c98daa53c1bcc435
Nixon Kosgei <nkosgey6@gmail.com>
2023-06-10 20:17:17 +0300

    reworked harry's solution

--- app/Http/Controllers/ussdMenuController.php
+++ app/Http/Controllers/ussdMenuController.php
@@ -2,191 +2,195 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 use App\Models\eventRegistration;
 use App\Http\Controllers\sendSMS\SmsAlertController;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
+use PHPUnit\Exception;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 
-class ussdMenuController extends Controller
-{
+
+class ussdMenuController extends Controller {
     //
 
-    public function index(Request $request)
-    {
+    private int $level;
+    private string $sessionId;
 
-        $sessionID = $request->input('SESSIONID');
-        $ussdCode = $request->input('USSDCODE');
+    private array $group1 = [
+        '1:Embakasi East',
+        '2:Embakasi West',
+        '3:Embakasi Central',
+        '4:Makadara',
+        '5:Kamukunji',
+        '6:Dagoretti South',
+        '7:Langata',
+        '8:Youth Alliance',
+        '9:MORE',
+    ];
+
+    private array $group2 = [
+        '10:Ruaraka',
+        '11:Roysambu',
+        '12:Embakasi North',
+        '13:Embakasi South',
+        '14:Northern Gate',
+        '15:Women Alliance',
+        '16:Dagoretti North',
+        '17:Starehe',
+        '18:MORE',
+    ];
+
+    private array $group3 = [
+        '19:Kahawa West',
+        '20:Eastern Gate',
+        '21:Kasarani',
+        '22:Mathare',
+        '23:Kibra',
+        '24:Westlands',
+        '25:Western Gate',
+    ];
+
+
+    public function index(Request $request) : Application|Response|\Illuminate\Contracts\Foundation\Application|ResponseFactory {
+        $this->sessionId = $request->input('SESSIONID');
+        //$ussdCode = $request->input('USSDCODE');
         $msisdn = $request->input('MSISDN');
         $input = $request->input('INPUT');
 
 
         $inputArray = explode("*", $input);
         $lastInput = end($inputArray);
-        $currentTime = Carbon::now();
+        // check if this is a new session
+        $isNewSession = !$request->session()->has($this->sessionId);
 
-        $group1 = [
-            '1:Embakasi East',
-            '2:Embakasi West',
-            '3:Embakasi Central',
-            '4:Makadara',
-            '5:Kamukunji',
-            '6:Dagoretti South',
-            '7:Langata',
-            '8:Youth Alliance',
-            '9:MORE'
-        ];
+        // level is how keep track of ussd sessions
+        $this->level = Screen::WELCOME;
+        if ($isNewSession) {
+            $request->session()->put($this->sessionId, Screen::WELCOME); // initialize session
+        } else {
+            $this->level = (int)$request->session()->get($this->sessionId); //fetch saved session
+        }
 
-        $group2 = [
-            '10:Ruaraka',
-            '11:Roysambu',
-            '12:Embakasi North',
-            '13:Embakasi South',
-            '14:Northern Gate',
-            '15:Women Alliance',
-            '16:Dagoretti North',
-            '17:Starehe',
-            '18:MORE'
-        ];
+        $result = match ($this->level) {
+            Screen::WELCOME => $this->welcomeScreen(),
+            Screen::REGISTER => $this->registerScreen($lastInput, $msisdn),
+            Screen::FULL_NAME => $this->fullNameScreen($lastInput),
+            Screen::CHURCH_NAME => $this->churchOrgScreen($lastInput),
+            Screen::ZONE_ONE => $this->zoneOneScreen($input),
+            Screen::ZONE_TWO => $this->zoneTwoScreen($input),
+            Screen::ZONE_THREE => $this->zoneThreeScreen($input),
+            default => "END menu is not set"
+        };
 
-        $group3 = [
-            '19:Kahawa West',
-            '20:Eastern Gate',
-            '21:Kasarani',
-            '22:Mathare',
-            '23:Kibra',
-            '24:Westlands',
-            '25:Western Gate'
-        ];
-        
-        
+        $request->session()->put($this->sessionId, $this->level); // save the next level
+        return response($result)->header('Content-Type', 'text/plain');
+    }
 
-        if ($lastInput == "76") {
+    private function welcomeScreen() : string {
+        $this->level = Screen::REGISTER; // go to the next screen
+        return "CON Welcome to Love Nairobi Festival Launch. Please select an option to Register" .
+            "\n1.LNF Pastors & Leaders Enrichment conference" .
+            "\n2.Kenya Students Christian Fellowship Nairobi County";
+    }
 
-            $response = "CON Welcome to Love Nairobi Festival Launch. Please select an option\n1.Register";
-            return response($response)->header('Content-Type', 'text/plain');
-        } elseif ($lastInput == "1") {
-
-
-            $mobile = DB::table('event_registrations')->where('mobile', $msisdn)->where('status', '1')->first();
-
-            if ($mobile) {
-                $response = "END You are already registered";
-                return response($response)->header('Content-Type', 'text/plain');
-            } else {
-
-                $mobile = DB::table('event_registrations')->where('mobile', $msisdn)->where('status', '0')->first();
-
-                if ($mobile) {
-
-                    if (!$mobile->Church_Name) {
-
-
-                        $response = "CON Enter Name Of Church/Organization represented";
-                        return response($response)->header('Content-Type', 'text/plain');
-                    } else if (!$mobile->Sub_County) {
-
-
-                        $response = "CON Choose a Zone:\n";
-                        foreach ($group1 as $group1) {
-                            $response .= $group1 . "\n";
-                        }
-
-                        return response($response)->header('Content-Type', 'text/plain');
-
-                        if ($lastInput == '9') {
-                            $response = "CON Choose a Zone:\n";
-                            foreach ($group2 as $group2) {
-                                $response .= $group2 . "\n";
-                            }
-                            
-                            return response($response)->header('Content-Type', 'text/plain');
-                        }elseif($lastInput == '18'){
-        
-                            $response = "CON Choose a Zone";
-        
-                            foreach ($group3 as $group3) {
-                                $response .= $group3 . "\n";
-                            }
-        
-                            return response($response)->header('Content-Type', 'text/plain');
-        
-                        }else{
-
-                            $response = "CON Enter Full Name";
-                            return response($response)->header('Content-Type', 'text/plain');
-                        }
-                    }
-
-                    
-                } else {
-
-                    DB::table('event_registrations')->insertOrIgnore(['mobile' => $msisdn]);
-                    $response = "CON Enter Full Name";
-                    return response($response)->header('Content-Type', 'text/plain');
-                }
+    private function registerScreen(int $input, $msisdn) : string {
+        if ($input == 1) {
+            $isUserRegistered = DB::table('event_registrations')
+                ->where('mobile', $msisdn)
+                ->where('status', '1')
+                ->first();
+            if ($isUserRegistered) {
+                return "END You are already registered";
             }
-        } elseif ($lastInput != '') {
+            session()->put("$this->sessionId:msisdn", $input);
+            $this->level = Screen::FULL_NAME;
+            return "CON Enter Full Name";
+        }
 
+        if ($input == 2) { //kenya students
+            // todo implement me
+            return "END hello student christian";
+        }
 
-            $registration = DB::table('event_registrations')->where('mobile', $msisdn)->where('status', '0')->first();
+        return "END Invalid option";
+    }
 
-            if (!$registration->name && !$registration->Church_Name && !$registration->Sub_County) {
+    private function fullNameScreen($input) : string {
+        session()->put("$this->sessionId:name", $input);
+        $this->level = Screen::CHURCH_NAME;
+        return "CON Enter Name Of Church/Organization represented";
+    }
 
-                DB::table('event_registrations')->where('mobile', $msisdn)->where('status', '0')->update(['name' => $lastInput, 'created_at' => $currentTime]);
+    private function churchOrgScreen($input) : string {
+        session()->put("$this->sessionId:church", $input);
+        $this->level = Screen::ZONE_ONE;
 
-                $response = "CON Enter Name Of Church/Organization represented";
+        $response = "CON Choose a Zone:\n";
+        foreach ($this->group1 as $group) {
+            $response .= $group . "\n";
+        }
+        return $response;
+    }
 
-                return  response($response)->header('Content-Type', 'text/plain');
-            } else if ($registration->name && !$registration->Church_Name && !$registration->Sub_County) {
-                DB::table('event_registrations')->where('mobile', $msisdn)->where('status', '0')->update(['Church_Name' => $lastInput, 'created_at' => $currentTime]);
-
-                $response = "CON Choose a Zone:\n";
-
-
-                foreach ($group1 as $group1) {
-                    $response .= $group1 . "\n";
-                }
-
-                return response($response)->header('Content-Type', 'text/plain');
-
-               
-            } else {
-
-                if ($lastInput == '9') {
-                    $response = "CON Choose a Zone:\n";
-                    foreach ($group2 as $group2) {
-                        $response .= $group2 . "\n";
-                    }
-                    
-                    return response($response)->header('Content-Type', 'text/plain');
-                }elseif($lastInput == '18'){
-
-                    $response = "CON Choose a Zone";
-
-                    foreach ($group3 as $group3) {
-                        $response .= $group3 . "\n";
-                    }
-
-                    return response($response)->header('Content-Type', 'text/plain');
-
-                }else{
-
-                    DB::table('event_registrations')->where('mobile', $msisdn)->where('status', '0')->update(['Sub_County' => $lastInput, 'status' => '1', 'created_at' => $currentTime]);
-                $sendSMS = new SmsAlertController();
-                $resp = $sendSMS->sendSMS($msisdn);
-    
-                $response = " END Registration Successful";
-                
-                return response($response)->header('Content-Type', 'text/plain');
-
-                }
-            
-                
-                }
-
-                
+    private function zoneOneScreen(string $input) : string {
+        if ($input == "9") {
+            $this->level = Screen::ZONE_TWO;
+            $response = "CON Choose a Zone:\n";
+            foreach ($this->group2 as $group) {
+                $response .= $group . "\n";
             }
+            return $response;
+        }
+        session()->put("$this->sessionId:zone", $input);
+
+        return $this->saveEvent();
+    }
+
+    private function zoneTwoScreen(string $input) : string {
+        if ($input == "18") {
+            $this->level = Screen::ZONE_THREE;
+            $response = "CON Choose a Zone:\n";
+            foreach ($this->group3 as $group) {
+                $response .= $group . "\n";
+            }
+            return $response;
+        }
+        session()->put("$this->sessionId:zone", $input);
+
+        return $this->saveEvent();
+    }
+
+    private function zoneThreeScreen(string $input) : string {
+        session()->put("$this->sessionId:zone", $input);
+        return $this->saveEvent();
+    }
+
+    private function saveEvent() : string {
+        try {
+            $msisdn = session()->get("$this->sessionId:msisdn");
+
+            $name = session()->get("$this->sessionId:name");
+            $church = session()->get("$this->sessionId:church");
+            $zone = session()->get("$this->sessionId:zone");
+            DB::table("event_registrations")->insertOrIgnore([
+                "mobile"      => $msisdn,
+                "name"        => $name,
+                "Sub_County"  => $zone,
+                "Church_Name" => $church,
+                "status"      => '1',
+                "created_at"  => Carbon::now(),
+            ]);
+
+            return "END Registration Successful";
+        } catch (Exception $e) {
+            return "END Error saving details, Try again";
         }
     }
+}
+
 
