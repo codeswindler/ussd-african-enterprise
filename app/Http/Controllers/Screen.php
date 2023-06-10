2f5d5f51da835ed16736034886c8ea40c63e7037
Nixon Kosgei <nkosgey6@gmail.com>
2023-06-10 20:10:07 +0300

    added screen keys

---
+++ app/Http/Controllers/Screen.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nixon
+ * Date: 10/6/2023
+ * Time: 19:04
+ */
+
+namespace App\Http\Controllers;
+
+class Screen {
+    const WELCOME = 1;
+    const REGISTER = 2;
+    const FULL_NAME = 2;
+    const CHURCH_NAME = 4;
+    const ZONE_ONE = 5;
+    const ZONE_TWO = 6;
+    const ZONE_THREE = 7;
+}
